741b4dcff76dcbcd492b45e74c024f09ab34e821
Malik Badreddine <malik.bad@gmail.com>
2014-03-17 13:08:33 +0100

    voyage a la carte partie mobile

---
+++ src/voyagealacarte/MidletVoyage.java
@@ -0,0 +1,30 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package voyagealacarte;
+
+import javax.microedition.lcdui.*;
+import javax.microedition.midlet.*;
+
+/**
+ * @author asus
+ */
+public class MidletVoyage extends MIDlet {
+    Display disp = Display.getDisplay(this);
+    Form f = new Form("f1");
+    Ticker t = new Ticker("Dexter:voyage a la carte");
+    
+    
+
+    public void startApp() {
+        f.setTicker(t);
+        disp.setCurrent(f);
+    }
+    
+    public void pauseApp() {
+    }
+    
+    public void destroyApp(boolean unconditional) {
+    }
+}
